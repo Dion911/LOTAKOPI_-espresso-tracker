950ed7dedc56bef5be8e3560568ddd109d905059
Dion911 <dionafe911@gmail.com>
2026-02-17 17:24:40 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# LOTAKOPI_-espresso-tracker
-LOTA KOPI espresso tracker
+# Espresso Tracker (PWA v0.2)
+
+Upload these files to a GitHub repo (root), enable GitHub Pages, then open the Pages URL on mobile and 'Add to Home Screen'.
+
+- Offline caching via sw.js
+- LocalStorage shot log + week summary
+- Export/Import JSON
---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE = "espresso-tracker-v0.2.1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./sw.js",
+  "./assets/helmet-girl.png",
+  "./assets/portafilter.png",
+  "./assets/tamper.png",
+  "./assets/icon-192.png",
+  "./assets/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
